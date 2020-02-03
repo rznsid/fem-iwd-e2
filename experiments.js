43e9f50659e6df4f25919ce819e1fa2eba06e036
rznsid <simon.whittenbury@gmail.com>
2020-02-03 21:24:36 +1100

    Corrected placement of experiment.js

--- experiments.js
+++ experiments.js
@@ -1,26 +1,25 @@
-window.onload = function () {
-    let result = document.querySelector('.result');
-    const calculator = document.querySelector('.calculator');
-    let firstNumber
-    let operator
+let result = document.querySelector('.result');
+const calculator = document.querySelector('.calculator');
+let firstNumber
+let operator
 
-    calculator.addEventListener('click', function (event) {
-        if (Number(event.target.innerText) | (event.target.innerText === '0')) {
-            updateNumber(result, event);
-        } else if (event.target.innerText === "C") {
-            result.innerText = "0";
-        } else if (event.target.innerText === "=") {
-            ({
-                operator,
-                firstNumber
-            } = calculateNumber(operator, result, firstNumber));
-        } else {
-            firstNumber = result.innerText;
-            operator = event.target.innerText;
-            result.innerText = "0";
-        }
-    });
-};
+calculator.addEventListener('click', function (event) {
+    if (Number(event.target.innerText) | (event.target.innerText === '0')) {
+        updateNumber(result, event);
+    } else if (event.target.innerText === "C") {
+        result.innerText = "0";
+    } else if (event.target.innerText === "=") {
+        ({
+            operator,
+            firstNumber
+        } = calculateNumber(operator, result, firstNumber));
+    } else {
+        firstNumber = result.innerText;
+        operator = event.target.innerText;
+        result.innerText = "0";
+    }
+});
+
 
 function calculateNumber(operator, result, firstNumber) {
     if (operator === "+") {
